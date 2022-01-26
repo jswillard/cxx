09f0311b1e10b7ee1ce5ccebbddcfb11dd496c57
Russell Greene <russellgreene8@gmail.com>
2022-01-26 12:22:04 -0600

    fix from_unmanaged not being implemented for C++ types, and add corresponding test

--- gen/src/write.rs
+++ gen/src/write.rs
@@ -1796,6 +1796,18 @@ fn write_shared_ptr(out: &mut OutFile, key: NamedImplKey) {
     begin_function_definition(out);
     writeln!(
         out,
+        "void cxxbridge1$shared_ptr${}$from_unmanaged(::std::shared_ptr<{}>* ptr, void* data) noexcept {{",
+        instance, inner,
+    );
+    writeln!(
+        out,
+        "new (ptr) std::shared_ptr<{}>(static_cast<{}*>(data));",
+        inner, inner
+    );
+    writeln!(out, "}}");
+    begin_function_definition(out);
+    writeln!(
+        out,
         "void cxxbridge1$shared_ptr${}$clone(const ::std::shared_ptr<{}>& self, ::std::shared_ptr<{}> *ptr) noexcept {{",
         instance, inner, inner,
     );
